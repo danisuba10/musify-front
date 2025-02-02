58fe1c8c43e979732c5bfcae43dbe793a9546783
Suba Dániel <danisuba10@gmail.com>
2025-02-02 23:32:44 +0200

    Add Vite SVGR to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: false, // Ensure it exports named components
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
