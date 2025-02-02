ddf2a267b7e2431d7b45135d9190a7119a32a823
Suba Dániel <danisuba10@gmail.com>
2025-02-02 23:33:59 +0200

    Make User library and Home scroll independently of eachother

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import "./App.css";
 import Home from "./components/homepage/home";
 import "./styles/tailwind.css";
+import UserLibrary from "./components/user_library/ULibrary";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className="top-parent">
+      <div className="top-bar"></div>
+      <div className="app-container">
+        <UserLibrary />
+        <Home />
+      </div>
+    </div>
   );
 }
 
