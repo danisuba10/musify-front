25bd0d230f3307bfc731c0f199f8125216534e59
Suba Dániel <danisuba10@gmail.com>
2025-02-15 00:50:33 +0200

    Connect AuthProvider to local API and add jwt-decode library to decode JWT

--- src/components/auth/AuthProvider.jsx
+++ src/components/auth/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export const AuthContext = createContext();
 
@@ -13,36 +14,108 @@ const AuthProvider = ({ children }) => {
     if (token) {
       setUserToken(token);
       setIsAuthenticated(true);
-      verifyRoles(token);
+      decodeToken(token);
     }
   }, []);
 
-  const verifyRoles = (token) => {
-    const adminStatus = token === "adminToken";
-    setIsAdmin(adminStatus);
-    sessionStorage.setItem("isAdmin", adminStatus);
+  const isTokenExpired = (token) => {
+    try {
+      const decoded = jwtDecode(token);
+      const currentTime = Date.now() / 1000;
+      return decoded.exp < currentTime;
+    } catch (error) {
+      console.error("Error decoding token!", error);
+      return true;
+    }
   };
 
-  const login = (email, password) => {
-    const token =
-      email === "admin@gmail.com" && password === "admin"
-        ? "adminToken"
-        : "userToken";
-    setUserToken(token);
-    sessionStorage.setItem("userToken", token);
-    setIsAuthenticated(true);
-    setUserInfo({ email });
-    verifyRoles(token);
+  const decodeToken = (token) => {
+    try {
+      const decoded = jwtDecode(token);
+      console.log("Decoded Token: ", decoded);
+      const isAdminBool = decoded?.role === "Admin";
+      setIsAdmin(isAdminBool);
+      setUserInfo(decoded);
+      sessionStorage.setItem("isAdmin", isAdmin);
+      console.log(isAdmin);
+    } catch (error) {
+      console.log("Error decoding token ", error);
+    }
   };
 
-  const register = (email, password) => {
-    const token = "userToken";
+  const handleValidToken = (token) => {
     setUserToken(token);
-    sessionStorage.setItem("userToken", token);
     setIsAuthenticated(true);
-    setUserInfo({ email });
-    setIsAdmin(false);
-    sessionStorage.setItem("isAdmin", false);
+    decodeToken(token);
+  };
+
+  //Interval possibly does not work
+  useEffect(() => {
+    const token = sessionStorage.getItem("userToken");
+    if (token) {
+      if (isTokenExpired(token)) {
+        console.log("Login expired 1!");
+        logout();
+      } else {
+        handleValidToken(token);
+      }
+
+      const interval = setInterval(() => {
+        const token = sessionStorage.getItem("userToken");
+        if (token && isTokenExpired(token)) {
+          console.log("Login expired 2!");
+          logout();
+        }
+      }, 5 * 60 * 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, []);
+
+  const login = async (email, password) => {
+    try {
+      const formData = new FormData();
+      formData.append("userName", email);
+      formData.append("password", password);
+
+      const response = await fetch("http://localhost:5231/user/login", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const token = await response.text();
+        sessionStorage.setItem("userToken", token);
+        handleValidToken(token);
+      } else {
+        console.error("Login failed!");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+
+  const register = async (email, password) => {
+    try {
+      const formData = new FormData();
+      formData.append("userName", email);
+      formData.append("password", password);
+
+      const response = await fetch("http://localhost:5231/user/register", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        const token = await response.text();
+        sessionStorage.setItem("userToken", token);
+        handleValidToken(token);
+      } else {
+        console.error("Register failed!");
+      }
+    } catch (error) {
+      console.error("Error during register:", error);
+    }
   };
 
   const logout = () => {
