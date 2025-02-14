f547b07f31cb62dbd965280ca0e18ddd080b1123
Suba Dániel <danisuba10@gmail.com>
2025-02-15 00:44:45 +0200

    Connect AddArtist to local API

--- src/components/AdminPanel/AddArtist.jsx
+++ src/components/AdminPanel/AddArtist.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import FormInput from "./FormInput";
+import { AuthContext } from "../auth/AuthProvider";
 
 const AddArtist = () => {
   const formFields = [
-    { label: "Artist name", name: "artistName", type: "text", required: true },
+    { label: "Artist name", name: "Name", type: "text", required: true },
     {
       label: "Artist image",
-      name: "artistImage",
+      name: "FormFile",
       type: "file",
       required: false,
     },
   ];
-  return <FormInput fields={formFields} />;
+
+  const handleSubmit = async (formData) => {
+    const userToken = useContext(AuthContext);
+    try {
+      // Log the form data content
+      for (let [key, value] of formData.entries()) {
+        console.log(key, value);
+      }
+      const response = await fetch("http://localhost:5231/artist/add-artist", {
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+
+      if (response.ok) {
+        console.log("Artist added successfully!");
+      } else {
+        console.error("Error adding artist");
+        console.log(response);
+      }
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+  return <FormInput fields={formFields} handleSubmit={handleSubmit} />;
 };
 
 export default AddArtist;
