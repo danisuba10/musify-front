79c8e6beac7a9fdca8e4a471cf6908c86cca25e1
Suba Dániel <danisuba10@gmail.com>
2025-02-19 00:12:59 +0200

    Completely modify search functions to return data if setSearchDisplay function not given

--- src/components/search/SearchFetches.jsx
+++ src/components/search/SearchFetches.jsx
@@ -2,7 +2,7 @@ import { React } from "react";
 import TableSearch from "./TableSearch";
 import { apiURL } from "../../assets/Constants";
 
-export const search = (
+export const search = async (
   setSearchDisplay,
   term,
   endPoint,
@@ -15,75 +15,84 @@ export const search = (
   formData.append("LastCreatedAt", "");
   formData.append("PageSize", 50);
 
-  setSearchDisplay(
-    <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
-      Loading..
-    </div>
-  );
+  if (setSearchDisplay) {
+    setSearchDisplay(
+      <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
+        Loading..
+      </div>
+    );
+  }
 
-  fetch(endPoint, {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          throw new Error("Not Found");
-        }
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("Data fetched: ", data);
-      if (
-        !data.searchResults ||
-        !data.searchResults.$values ||
-        data.searchResults.$values.length === 0
-      ) {
-        setSearchDisplay(
-          <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
-            No {title.toLowerCase()} found for "{term}".
-          </div>
-        );
-      } else {
-        const albums = data.searchResults.$values.map((album) => ({
-          id: album.id,
-          name: album.name,
-          image: `${apiURL}/image/${encodeURIComponent(album.imageLocation)}`,
-        }));
-        setSearchDisplay(
-          <TableSearch
-            title={title}
-            elements={albums}
-            type={rounded ? "circle" : ""}
-          />
-        );
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching albums:", error);
-      if (error.message === "Not Found") {
-        setSearchDisplay(
-          <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
-            No {title.toLowerCase()} found for "{term}".
-          </div>
-        );
-      } else {
-        setSearchDisplay(
-          <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
-            An error occurred while fetching {title.toLowerCase()}. Error:{" "}
-            {error.message}
-          </div>
-        );
-      }
+  try {
+    const response = await fetch(endPoint, {
+      method: "POST",
+      body: formData,
     });
-};
 
-export const searchArtists = () => {
-  const formData = new FormData();
-  formData.append("SearchTerm", term);
-  formData.append("LastName", "");
-  formData.append("LastCreatedAt", "");
-  formData.append("PageSize", 50);
+    if (!response.ok) {
+      if (response.status === 404) {
+        throw new Error("Not Found");
+      }
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Data fetched: ", data);
+
+    if (
+      !data.searchResults?.$values ||
+      data.searchResults.$values.length === 0
+    ) {
+      if (setSearchDisplay) {
+        setSearchDisplay(
+          <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
+            No {title.toLowerCase()} found for "{term}".
+          </div>
+        );
+        return [];
+      }
+      return [];
+    }
+
+    const albums = data.searchResults.$values.map((album) => ({
+      id: album.id,
+      name: album.name,
+      image: `${apiURL}/image/${encodeURIComponent(album.imageLocation)}`,
+    }));
+
+    console.log("Results before returning:", albums);
+
+    if (setSearchDisplay) {
+      setSearchDisplay(
+        <TableSearch
+          title={title}
+          elements={albums}
+          type={rounded ? "circle" : ""}
+        />
+      );
+    }
+    return albums;
+  } catch (error) {
+    console.error("Error fetching:", error);
+    if (error.message === "Not Found") {
+      if (setSearchDisplay) {
+        setSearchDisplay(
+          <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
+            No {title.toLowerCase()} found for "{term}".
+          </div>
+        );
+      }
+      return [];
+    }
+
+    if (setSearchDisplay) {
+      setSearchDisplay(
+        <div className="text-white text-3xl font-bold ml-10 w-full h-auto">
+          An error occurred while fetching {title.toLowerCase()}. Error:{" "}
+          {error.message}
+        </div>
+      );
+    }
+    throw error;
+  }
 };
