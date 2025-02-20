fdbe33f5b17f9d47a34ee86d6090c41fec356b25
Suba Dániel <danisuba10@gmail.com>
2025-02-21 00:11:09 +0200

    Refactor ProfileDetail component to accept type prop and update HorizontalScrollGrid to display Top Albums with initial elements

--- src/components/details/ProfileDetail/ProfileDetail.jsx
+++ src/components/details/ProfileDetail/ProfileDetail.jsx
@@ -10,14 +10,14 @@ import HorizontalScrollGrid from "../../homepage/HorizontalScrollGrid";
 // import CollectionDetailActionBar from "./CollectionDetailActionBar";
 // import CollectionDetailList from "./CollectionDetailList";
 
-export default function ProfileDetail({ profile }) {
+export default function ProfileDetail({ profile, type = "Profile" }) {
   return (
     <div className="profile-container">
       <div className="overflow-y-scroll overflow-x-hidden scroll-smooth h-full">
         <CollectionDetailCard
           collection={profile}
           typeCSS="circle"
-          type="user"
+          type={type}
         />
         <ProfileActionBar
           middleColor={profile.colors.middle}
@@ -25,23 +25,10 @@ export default function ProfileDetail({ profile }) {
         />
         <div className="mt-6"></div>
         <HorizontalScrollGrid
-          title="Followers"
-          type="circle"
-          artists={artists}
-          route="/profile/"
-        />
-        <HorizontalScrollGrid
-          title="Playlists"
-          artists={artists}
-          route="/plalist/"
+          title="Top Albums"
+          initialElements={profile.albums}
         />
         <div className="mt-16"></div>
-        <div
-          className="h-[4vh] w-full"
-          style={{
-            backgroundImage: "linear-gradient(to bottom, #2a162c, #201c1c)",
-          }}
-        ></div>
       </div>
     </div>
   );
