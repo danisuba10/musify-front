9959c6a7db40e62ffc9f94afc651f7c371d9f747
Suba Dániel <danisuba10@gmail.com>
2025-02-21 15:49:50 +0200

    Add album search functionality to ModifyAlbum component

--- src/components/AdminPanel/ModifyAlbum.jsx
+++ src/components/AdminPanel/ModifyAlbum.jsx
@@ -1,19 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import FormInput from "./FormInput";
 import AddSong from "./AddSong";
 import { AuthContext } from "../auth/AuthProvider";
 import ViewAlbum from "./ViewAlbum";
 import { emptyAlbum } from "../../assets/Constants";
+import Search from "../search/Search";
 
 const ModifyAlbum = ({ searchTerm }) => {
+  const [showAlbumSearch, setShowAlbumSearch] = useState(false);
+  const [albumId, setAlbumId] = useState(null);
+  const [currentSelectionFunc, setCurrentSelectionFunc] = useState(null);
+
+  const onChangeAlbumClick = () => {
+    setShowAlbumSearch(true);
+  };
+
+  const selectAlbum = (album) => {
+    setAlbumId(album.id);
+  };
+
   return (
     <>
-      <ViewAlbum
-        id={"147a20f8-36cd-4893-947d-54d8a205052f"}
-        searchTerm={searchTerm}
-        isModify={true}
-      />
+      {showAlbumSearch && (
+        <div className="w-[95%]">
+          <Search
+            initialTerm={searchTerm}
+            selectionFunc={(album) => {
+              selectAlbum(album);
+              setShowAlbumSearch(false);
+            }}
+            defaultFilter="Albums"
+            onlyFilter={true}
+          />
+        </div>
+      )}
+      {!showAlbumSearch && (
+        <>
+          <button
+            className="flex relative p-2 rounded-3xl border-2 border-white mt-4"
+            onClick={onChangeAlbumClick}
+          >
+            <span className="text-white font-bold">Change album</span>
+          </button>
+          <ViewAlbum id={albumId} searchTerm={searchTerm} isModify={true} />
+          <div className="mt-28"></div>
+        </>
+      )}
     </>
   );
 };
