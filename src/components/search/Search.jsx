90ea416bb6c23bae59d50b2e28c187f0fd8af14c
Suba Dániel <danisuba10@gmail.com>
2025-02-22 22:32:35 +0200

    Set default value for selectionFunc prop in Search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -14,7 +14,7 @@ import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Search({
   initialTerm,
-  selectionFunc,
+  selectionFunc = null,
   defaultFilter = "All",
   onlyFilter = false,
   setIsSearch,
