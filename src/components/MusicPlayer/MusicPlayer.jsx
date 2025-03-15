3b1c7dd4dcce0475a01f261fc5f8aa1f3255887e
Suba Dániel <danisuba10@gmail.com>
2025-03-15 19:06:17 +0200

    Responsive modifications for mobile

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -64,6 +64,18 @@ const MusicPlayer = ({ songId }) => {
 
         if (response.ok) {
           const data = await response.json();
+          var newTitle = "";
+          if (isMobile) {
+            newTitle =
+              data.song.title.length > 234
+                ? `${data.song.title.slice(0, 234)}...`
+                : data.song.title;
+          } else {
+            newTitle =
+              data.song.title.length > 68
+                ? `${data.song.title.slice(0, 68)}...`
+                : data.song.title;
+          }
           setSongInfo({
             streamUrl: `${apiURL}/sound/${encodeURIComponent(
               data.songFileUrl
@@ -152,7 +164,7 @@ const MusicPlayer = ({ songId }) => {
   };
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 px-4 py-2 flex items-center z-50">
+    <div className="fixed h-[25vh] md:h-[15vh] lg:h-[10vh] bottom-0 left-0 right-0 bg-black border-t border-gray-800 px-4 py-2 flex items-center z-50">
       <audio
         ref={audioRef}
         src={songInfo?.streamUrl}
@@ -161,81 +173,104 @@ const MusicPlayer = ({ songId }) => {
         onLoadedMetadata={handleTimeUpdate}
       />
 
-      <div className="flex w-full justify-between">
-        <div className="flex relative items-center h-full w-1/4">
-          {songInfo?.image && (
-            <img
-              src={songInfo.image}
-              alt="Album cover"
-              className="h-12 w-12 object-cover mr-3"
-            />
-          )}
-          <div className="flex flex-col">
-            <span className="text-white text-sm font-medium truncate">
-              {songInfo?.songTitle || "No track selected"}
-            </span>
-            <span className="text-gray-400 text-xs truncate">
-              {songInfo?.artists?.join(", ") || ""}
-            </span>
-          </div>
-        </div>
-        <div className="flex flex-col w-full">
-          <div className="flex items-center justify-center mb-2 w-full">
-            {/* Playback controls */}
-            <div className="flex items-center justify-center space-x-4">
-              <button className="text-gray-400 hover:text-white focus:outline-none">
-                <SkipBack size={20} />
-              </button>
-              <button
-                className="text-white p-1 rounded-full bg-gray-800 hover:bg-gray-700 focus:outline-none"
-                onClick={togglePlay}
-              >
-                {isPlaying ? <Pause size={24} /> : <Play size={24} />}
-              </button>
-              <button className="text-gray-400 hover:text-white focus:outline-none">
-                <SkipForward size={20} />
-              </button>
+      <div className="flex flex-col h-full w-full justify-start">
+        {isMobile && (
+          <div className="w-full flex relative items-center h-min">
+            {songInfo?.image && (
+              <img
+                src={songInfo.image}
+                alt="Album cover"
+                className="h-12 w-12 object-cover mr-3"
+              />
+            )}
+            <div className="flex flex-col w-full h-full">
+              <span className="w-full text-white text-sm font-medium break-all">
+                {songInfo?.songTitle || "No track selected"}
+              </span>
+              <span className="text-gray-400 text-xs truncate">
+                {songInfo?.artists?.join(", ") || ""}
+              </span>
             </div>
-            {/* Volume control */}
-          </div>
-
-          {/* Progress bar row */}
-          <div className="flex items-center justify-center space-x-3 w-full">
-            <span className="text-xs text-gray-400 w-10 text-right">
-              {formatTime(currentTime)}
-            </span>
-            <input
-              type="range"
-              min="0"
-              max={duration || 0}
-              value={currentTime}
-              onChange={handleSeek}
-              className="flex w-[40%] h-1 bg-gray-600 rounded-lg cursor-pointer accent-green-500"
-            />
-            <span className="text-xs text-gray-400 w-10">
-              {formatTime(duration)}
-            </span>
-          </div>
-        </div>
-        {!isMobile && (
-          <div className="flex items-center w-1/4 justify-end">
-            <button
-              className="text-gray-400 hover:text-white focus:outline-none mr-2"
-              onClick={toggleMute}
-            >
-              {isMuted ? <VolumeX size={20} /> : <Volume2 size={20} />}
-            </button>
-            <input
-              type="range"
-              min="0"
-              max="1"
-              step="0.01"
-              value={isMuted ? 0 : volume}
-              onChange={handleVolumeChange}
-              className="w-24 h-1 bg-gray-600 rounded-lg cursor-pointer accent-green-500"
-            />
           </div>
         )}
+        <div className="flex w-full">
+          {!isMobile && (
+            <div className="flex relative items-center h-full w-1/3">
+              {songInfo?.image && (
+                <img
+                  src={songInfo.image}
+                  alt="Album cover"
+                  className="h-12 w-12 object-cover mr-3"
+                />
+              )}
+              <div className="flex flex-col">
+                <span className="text-white text-sm font-medium break-all">
+                  {songInfo?.songTitle || "No track selected"}
+                </span>
+                <span className="text-gray-400 text-xs break-all">
+                  {songInfo?.artists?.join(", ") || ""}
+                </span>
+              </div>
+            </div>
+          )}
+          <div className="flex flex-col w-full">
+            <div className="flex items-center justify-center mb-2 w-full">
+              {/* Playback controls */}
+              <div className="flex items-center justify-center space-x-4">
+                <button className="text-gray-400 hover:text-white focus:outline-none">
+                  <SkipBack size={20} />
+                </button>
+                <button
+                  className="text-white p-1 rounded-full bg-gray-800 hover:bg-gray-700 focus:outline-none"
+                  onClick={togglePlay}
+                >
+                  {isPlaying ? <Pause size={24} /> : <Play size={24} />}
+                </button>
+                <button className="text-gray-400 hover:text-white focus:outline-none">
+                  <SkipForward size={20} />
+                </button>
+              </div>
+              {/* Volume control */}
+            </div>
+
+            {/* Progress bar row */}
+            <div className="flex items-center justify-center space-x-3 w-full">
+              <span className="text-xs text-gray-400 w-10 text-right">
+                {formatTime(currentTime)}
+              </span>
+              <input
+                type="range"
+                min="0"
+                max={duration || 0}
+                value={currentTime}
+                onChange={handleSeek}
+                className="flex w-[40%] h-1 bg-gray-600 rounded-lg cursor-pointer accent-green-500"
+              />
+              <span className="text-xs text-gray-400 w-10">
+                {formatTime(duration)}
+              </span>
+            </div>
+          </div>
+          {!isMobile && (
+            <div className="flex items-center w-1/4 justify-end">
+              <button
+                className="text-gray-400 hover:text-white focus:outline-none mr-2"
+                onClick={toggleMute}
+              >
+                {isMuted ? <VolumeX size={20} /> : <Volume2 size={20} />}
+              </button>
+              <input
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={isMuted ? 0 : volume}
+                onChange={handleVolumeChange}
+                className="w-24 h-1 bg-gray-600 rounded-lg cursor-pointer accent-green-500"
+              />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
